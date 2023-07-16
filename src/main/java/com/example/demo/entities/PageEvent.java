e677e0d97c8227350ee86e04b8b26736a07825a9
YASSINE LAMBARKI <39433462+developpeurk@users.noreply.github.com>
2023-07-16 20:04:07 +0100

    kafka broker with spring cloud stream

---
+++ src/main/java/com/example/demo/entities/PageEvent.java
@@ -0,0 +1,21 @@
+package com.example.demo.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class PageEvent {
+
+    private String name;
+    private String username;
+    private Date date;
+    private long duration;
+
+}
